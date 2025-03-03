52b6799c851281294d63f861c24119dd0311e410
laur <112447573+lalallaur@users.noreply.github.com>
2025-03-02 23:40:20 -0800

    Create OutOfStockException.java

---
+++ Lab4.3Concurrency/OutOfStockException.java
@@ -0,0 +1,12 @@
+package edu.sdccd.cisc191;
+
+/**
+ * The class is used when a Warehouse does not have any Items in stock.
+ *
+ * Adapted from Tasha Frankie and Allan Schougaard
+ */
+public class OutOfStockException extends Exception
+{
+    public OutOfStockException(String s) {
+    }
+}
